7672dae248ea3db31f2efbd72202a5b009d4868a
suzakutakumi <suzakutakumi3@gmail.com>
2024-06-24 16:22:06 +0900

    qr_reader.pyを参照するように変更した

--- scan_qr/scan_qr.py
+++ scan_qr/scan_qr.py
@@ -6,7 +6,11 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
-from .. import qr_reader
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+import qr_reader
 
 class QRSubscriber(Node):
 
@@ -29,30 +33,7 @@ class QRSubscriber(Node):
             print(e)
             return
         
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-
-        qr_code_detector = cv2.QRCodeDetector()
-
-        # QRコードを検出
-        data, bboxes, _ = qr_code_detector.detectAndDecode(cv_image)
-
-        # QRコードが検出された場合
-        if bboxes is not None:
-            # QRコードの内容を出力
-            print(f'QRコードの内容: {data}')
-            bbox=bboxes[0]
-
-            if len(bbox) == 4 and all(len(box) == 2 for box in bbox):
-                for i in range(len(bbox)):
-                    # bboxの点を整数に変換
-                    point1 = tuple(map(int,bbox[i]))
-                    point2 = tuple(map(int, bbox[(i + 1) % len(bbox)]))
-
-                    # 線を引く
-                    cv2.line(cv_image, point1, point2, (0, 255, 0), 2)
-                
-                # QRコードの内容を画像に描画
-                cv2.putText(cv_image, data, (bbox[0][0].astype(int), bbox[0][1].astype(int) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        qr_reader.scan_qr(cv_image)
         
         try:
             ros_image=self.bridge.cv2_to_imgmsg(cv_image,'rgb8')
