52666002695f72aa76230beb8fc22a78cedaf66c
suzakutakumi <suzakutakumi3@gmail.com>
2024-07-12 12:44:09 +0900

    複数の画像を読み込めるようにした

--- qr_reader.py
+++ qr_reader.py
@@ -1,15 +1,21 @@
+import os
 import cv2
-from pyzbar.pyzbar import decode, ZBarSymbol
+from pyzbar import pyzbar
 
 OPENCV = "opencv"
 ZBAR = "zbar"
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+RESET_COLOR = '\033[0m'
 
 
 def scan_qr(image, library_type=OPENCV):
 
     if library_type == OPENCV:
         # グレースケールに変換
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
         # QRコードリーダーを作成
         qr_code_detector = cv2.QRCodeDetectorAruco()
@@ -20,7 +26,7 @@ def scan_qr(image, library_type=OPENCV):
         # QRコードが検出された場合
         if bbox is not None:
             # QRコードの内容を出力
-            print(f'QRコードの内容: {data}')
+            print(f'{GREEN if data!="" else YELLOW}QRコードの内容: {data}{RESET_COLOR}')
 
             box = bbox[0]
             if len(box) == 4 and all(len(b) == 2 for b in box):
@@ -36,28 +42,32 @@ def scan_qr(image, library_type=OPENCV):
                     cv2.putText(image, data, (box[0][0].astype(int), box[0][1].astype(
                         int) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         else:
-            print('QRコードが読み取れませんでした')
+            print(f'{RED}QRコードが読み取れませんでした{RESET_COLOR}')
         return image
     elif library_type == ZBAR:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        value = decode(gray, symbols=[ZBarSymbol.QRCODE])
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        value = pyzbar.decode(gray, symbols=[pyzbar.ZBarSymbol.QRCODE])
 
         if value:
-            qrcode = value[0]
+            data = ""
+            for qrcode in value:
+                data += qrcode.data.decode('utf-8')+","
+
+                # QRコード座標取得
+                x, y, w, h = qrcode.rect
+
+                # QRコードデータ
+                cv2.putText(image, data, (x, y - 6),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+                # バウンディングボックス
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # QRコードの内容を出力
-            data = qrcode.data.decode('utf-8')
-            print(f'QRコードの内容: {data}')
+            print(f'{GREEN}QRコードの内容: {data}{RESET_COLOR}')
 
-            # QRコード座標取得
-            x, y, w, h = qrcode.rect
-
-            # QRコードデータ
-            cv2.putText(image, data, (x, y - 6),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-            # バウンディングボックス
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        else:
+            print(f'{RED}QRコードが読み取れませんでした{RESET_COLOR}')
         return image
     else:
         print(f'ライブラリ{library_type}は存在しません')
@@ -65,9 +75,26 @@ def scan_qr(image, library_type=OPENCV):
 
 if __name__ == "__main__":
     # 画像ファイルのパス
-    image_path = '####.png'
+    image_dir = "QR_image"
+    image_dir_list = [os.path.join(image_dir, file)
+                      for file in os.listdir(image_dir)]
 
-    # 画像を読み込む
-    image = cv2.imread(image_path)
+    # scanの設定
+    lib_type = OPENCV
 
-    scan_qr(image)
+    print(image_dir+"/")
+    for image_path in [file for file in image_dir_list if os.path.isfile(file)]:
+        image = cv2.imread(image_path)
+        print("  "+image_path[len(image_dir)+1:]+":\t", end="")
+        scan_qr(image, lib_type)
+    print()
+
+    for dir in [dir for dir in image_dir_list if os.path.isdir(dir)]:
+        print(dir+"/")
+        for file_name in os.listdir(dir):
+            image_path = os.path.join(dir, file_name)
+            if os.path.isfile(image_path) and file_name.endswith((".png", ".jpg", ".jpeg")):
+                image = cv2.imread(image_path)
+                print("  "+file_name+":\t", end="")
+                scan_qr(image, lib_type)
+        print()
