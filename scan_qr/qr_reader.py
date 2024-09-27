f857596796a6af4f3966cda71cf18ce2df2f3d5a
suzakutakumi <suzakutakumi3@gmail.com>
2024-09-27 11:07:51 +0900

    雑多に修正した
    - ROS2からOpenCVの画像変換をbgr8に修正
    - 画像のリサイズを追加
    - QRの内容を送るPublisherを追加

--- scan_qr/qr_reader.py
+++ scan_qr/qr_reader.py
@@ -11,6 +11,13 @@ YELLOW = '\033[33m'
 RESET_COLOR = '\033[0m'
 
 
+def image_resize(image, width=1280):
+    h, w = image.shape[:2]
+    height = round(h * (width / w))
+    image = cv2.resize(image, (width, height),interpolation=cv2.INTER_LINEAR)
+    return image
+
+
 def convert_image(origin, thresh=21):
     gray = cv2.cvtColor(origin, cv2.COLOR_BGR2GRAY)
     img = cv2.adaptiveThreshold(
@@ -19,7 +26,9 @@ def convert_image(origin, thresh=21):
     return img
 
 
-def scan_qr(image, library_type=OPENCV):
+def scan_qr(image, library_type=OPENCV, original=[]):
+    if original == []:
+        original = image.copy()
 
     if library_type == OPENCV:
 
@@ -40,14 +49,14 @@ def scan_qr(image, library_type=OPENCV):
                     point2 = tuple(map(int, box[(i + 1) % len(box)]))
 
                     # 線を引く
-                    cv2.line(image, point1, point2, (0, 255, 0), 2)
+                    cv2.line(original, point1, point2, (0, 255, 0), 2)
 
                     # QRコードの内容を画像に描画
-                    cv2.putText(image, data, (box[0][0].astype(int), box[0][1].astype(
+                    cv2.putText(original, data, (box[0][0].astype(int), box[0][1].astype(
                         int) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            return image, [data]
+            return original, [data]
         else:
-            return image, []
+            return original, []
 
     elif library_type == ZBAR:
         value = pyzbar.decode(image, symbols=[pyzbar.ZBarSymbol.QRCODE])
@@ -62,23 +71,23 @@ def scan_qr(image, library_type=OPENCV):
                 x, y, w, h = qrcode.rect
 
                 # QRコードデータ
-                cv2.putText(image, v, (x, y - 6),
+                cv2.putText(original, v, (x, y - 6),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
                 # バウンディングボックス
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.rectangle(original, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            return image, data
+            return original, data
         else:
-            return image, []
+            return original, []
     else:
         print(f'ライブラリ{library_type}は存在しません')
-        return image, []
+        return original, []
 
 
 if __name__ == "__main__":
     # 画像ファイルのパス
-    image_dir = "QR_image"
+    image_dir = "../QR_image"
     image_dir_list = [os.path.join(image_dir, file)
                       for file in os.listdir(image_dir)]
 
@@ -87,7 +96,7 @@ if __name__ == "__main__":
 
     print(image_dir+"/")
     for image_path in [file for file in image_dir_list if os.path.isfile(file)]:
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(image_path)
         image = convert_image(image, 51)
         print("  "+image_path[len(image_dir)+1:]+":\t", end="")
         _, v = scan_qr(image, lib_type)
@@ -105,7 +114,7 @@ if __name__ == "__main__":
         for file_name in os.listdir(dir):
             image_path = os.path.join(dir, file_name)
             if os.path.isfile(image_path) and file_name.endswith((".png", ".jpg", ".jpeg")):
-                image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+                image = cv2.imread(image_path)
                 image = convert_image(image, 51)
                 print("  "+file_name+":\t", end="")
                 _, v = scan_qr(image, lib_type)
--- scan_qr/scan_qr.py
+++ scan_qr/scan_qr.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
 import cv2
@@ -8,11 +9,13 @@ from cv_bridge import CvBridge, CvBridgeError
 
 from scan_qr import qr_reader
 
+
 class QRSubscriber(Node):
 
     def __init__(self):
         super().__init__('qr_subscriber')
-        self.publisher_ = self.create_publisher(Image, 'qr_image', 10)
+        self.image_publisher = self.create_publisher(Image, 'qr_image', 10)
+        self.value_publisher = self.create_publisher(String, 'qr_value', 10)
         self.subscription = self.create_subscription(
             Image,
             'image_raw',
@@ -24,23 +27,33 @@ class QRSubscriber(Node):
     def listener_callback(self, msg):
         try:
             # ROS2のsensor_msgs/Image型からOpenCVで処理適量にcv::Mat型へ変換する。
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
             print(e)
             return
 
-        cv_image, data = qr_reader.scan_qr(qr_reader.convert_image(cv_image))
+        cv_image = qr_reader.image_resize(cv_image)
+
+        original_image = cv_image.copy()
+        processed_image, data = qr_reader.scan_qr(
+            qr_reader.convert_image(cv_image), qr_reader.OPENCV, original_image)
         if data == []:
             print(f'{qr_reader.RED}読み取れませんでした{qr_reader.RESET_COLOR}')
         elif data[0] == "":
             print(f'{qr_reader.YELLOW}内容がわかりませんでした{qr_reader.RESET_COLOR}')
         else:
-            text=', '.join(data)
+            text = ', '.join(data)
             print(f'{qr_reader.GREEN}内容:{text}{qr_reader.RESET_COLOR}')
 
+        text = ','.join(data)
+        if text != "":
+            pub_data = String()
+            pub_data.data = ','.join(data)
+            self.value_publisher.publish(pub_data)
+
         try:
-            ros_image = self.bridge.cv2_to_imgmsg(cv_image, 'mono8')
-            self.publisher_.publish(ros_image)
+            ros_image = self.bridge.cv2_to_imgmsg(processed_image, 'bgr8')
+            self.image_publisher.publish(ros_image)
         except CvBridgeError as e:
             print(e)
             return
